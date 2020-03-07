cc096455d6ecb977cac1ba19724f52fe7aa338c7
jgordinier <jg@allmail.net>
2020-03-06 21:23:27 -0600

    4.2.2 HTTP

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -5,6 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Array exposing (Array)
+import Http
 import Random
 
 urlPrefix : String
